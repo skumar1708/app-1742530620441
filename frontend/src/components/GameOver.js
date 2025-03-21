f399c09bbb237ac085174f5d9b94520967812a17
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 09:50:52 +0530

    Automated commit: frontend/src/components/GameOver.js

---
+++ frontend/src/components/GameOver.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function GameOver() {
+  return (
+    <div className="game-over">
+      <h1>Game Over</h1>
+    </div>
+  );
+}
+
+export default GameOver;
